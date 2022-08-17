2951ec333660dcb0658f79bc6232ef4b7fe47127
jeremy-tesh <jermitesh20@gmail.com>
2022-08-17 20:48:55 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre)=>{
+const accounts = await hre.ethers.getSigner();
+
+  for (const account of accounts){
+    console.log(account.address)
+  }
+
+})
+
+
+
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
