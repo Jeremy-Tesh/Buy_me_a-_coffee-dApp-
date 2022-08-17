fee65471a02654445be049a3ee71c1fba5ff9b87
jeremy-tesh <jermitesh20@gmail.com>
2022-08-17 13:07:09 +0300

    fix:deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const main = async () => {
-    const [deployer] =await hexStripZeros.ethers.getSigners();
+    const [deployer] =await hre.ethers.getSigners();
     const accountBalance = await deployer.getBalance();
 
 
@@ -9,8 +9,24 @@ const main = async () => {
 
 
 
-    const Token  = await hre.etheres.getcontractFactory('CoffeePortal')
+    const Token  = await hre.ethers.getContractFactory('CoffeePortal')
     const portal = await Token.deploy()
     await portal.deployed();
 
+    console.log("CoffeePortal address:", portal.address)
+
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0)
+        
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+runMain();
 
