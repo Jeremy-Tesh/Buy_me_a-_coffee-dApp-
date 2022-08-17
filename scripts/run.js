fa8b2d7527865e39605171cb0b0ca1381e3faa20
jeremy-tesh <jermitesh20@gmail.com>
2022-08-17 15:13:31 +0300

    feat:run testing smart contract functions

--- scripts/run.js
+++ scripts/run.js
@@ -9,6 +9,25 @@ const main = async () => {
     console.log("Coffee contract delpoyed:", coffeeContract.address)
 };
 
+
+let contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address)
+console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance))
+
+
+
+const coffeeTxn = await coffeeContract.buyCoffee(
+    "coffee #1",
+    "Ermias",
+    ethers.utils.parseEther("0.001")
+);
+await coffeeTxn.wait();
+
+
+let allCoffee =  await coffeeContract.getAllCoffee();
+
+
+
+
 const runMain = async () => {
     try {
         await main();
